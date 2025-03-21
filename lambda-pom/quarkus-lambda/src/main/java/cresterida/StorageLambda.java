4e4c1b797a8ec98b08fe20193f712893c3bb86ed
kiquetal <kiquetal@gmail.com>
2025-03-21 15:18:31 -0300

    Adding env variable to be used by sam local invoke

--- lambda-pom/quarkus-lambda/src/main/java/cresterida/StorageLambda.java
+++ lambda-pom/quarkus-lambda/src/main/java/cresterida/StorageLambda.java
@@ -4,10 +4,9 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import jakarta.inject.Named;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
-import software.amazon.awssdk.profiles.ProfileFile;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3ClientBuilder;
@@ -17,27 +16,44 @@ import java.net.URI;
 @Named("s3")
 public class StorageLambda implements RequestHandler<InputObject, OutputObject>
 {
+
+    private final S3Client s3Client;
+
+    public StorageLambda() {
+        S3ClientBuilder s3ClientBuilder = S3Client.builder()
+                .httpClientBuilder(ApacheHttpClient.builder())
+                .region(Region.US_EAST_1);
+
+        if (isLocalEnvironment()) {
+            s3ClientBuilder.endpointOverride(URI.create("http://localhost.localstack.cloud:4566"))
+                    .forcePathStyle(true)
+                    .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test")));
+        } else {
+            s3ClientBuilder.credentialsProvider(DefaultCredentialsProvider.create());
+        }
+        this.s3Client = s3ClientBuilder.build();
+    }
+
+
     @Override
     public OutputObject handleRequest(InputObject input, Context context) {
         OutputObject out = new OutputObject();
 
-        S3Client s3Client = S3Client.builder()
-                .httpClientBuilder(ApacheHttpClient.builder())
-                        .region(Region.US_EAST_1)
-                           //     .endpointOverride(URI.create("http://localhost:4566"))
-                .endpointOverride(URI.create("http://localhost.localstack.cloud:4566"))
-                .forcePathStyle(true)
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test")))
-
-                        .build();
-
-
-        s3Client.listBuckets().buckets().forEach(bucket -> {
-            System.out.println(bucket.name());
-        });
-        out.setResult("This is after executing s3");
+        try {
+            s3Client.listBuckets().buckets().forEach(bucket -> {
+                System.out.println(bucket.name());
+            });
+            out.setResult("Successfully listed S3 buckets.");
+        } catch (Exception e) {
+            System.err.println("Error listing S3 buckets: " + e.getMessage());
+            out.setResult("Error executing s3: " + e.getMessage());
+        }
 
         return out;
+    }
 
+    private boolean isLocalEnvironment() {
+        System.out.println(System.getenv("AWS_SAM_LOCAL"));
+        return System.getenv("AWS_SAM_LOCAL") != null;
     }
 }
